3da005589464d3d4186ef53f62133c6198cefaee
Edson Camargo Menezes <42123898+edsoncamargo@users.noreply.github.com>
2019-11-01 10:48:06 -0300

    .
    .

--- src/main/java/br/senac/backend/model/games/Game.java
+++ src/main/java/br/senac/backend/model/games/Game.java
@@ -1,8 +1,7 @@
 package br.senac.backend.model.games;
 
-
 public class Game {
-	
+
 	private Integer id;
 	private String nome;
 	private Double preco;
@@ -10,51 +9,51 @@ public class Game {
 	private String desenvolvedor;
 	private String descricao;
 	private Integer idCategoria;
-	
+
 	public Integer getId() {
 		return id;
 	}
-	
+
 	public void setId(Integer id) {
 		this.id = id;
 	}
-	
+
 	public String getNome() {
 		return nome;
 	}
-	
+
 	public void setNome(String nome) {
 		this.nome = nome;
 	}
-	
+
 	public Double getPreco() {
 		return preco;
 	}
-	
+
 	public void setPreco(Double preco) {
 		this.preco = preco;
 	}
-	
+
 	public String getDesenvolvedor() {
 		return desenvolvedor;
 	}
-	
+
 	public void setDesenvolvedor(String desenvolvedor) {
 		this.desenvolvedor = desenvolvedor;
 	}
-	
+
 	public String getDescricao() {
 		return descricao;
 	}
-	
+
 	public void setDescricao(String descricao) {
 		this.descricao = descricao;
 	}
-	
+
 	public Integer getIdCategoria() {
 		return idCategoria;
 	}
-	
+
 	public void setIdCategoria(Integer idCategoria) {
 		this.idCategoria = idCategoria;
 	}
@@ -66,5 +65,12 @@ public class Game {
 	public void setDataLancamento(String dataLancamento) {
 		this.dataLancamento = dataLancamento;
 	}
-	
+
+	@Override
+	public String toString() {
+		return "Game [id=" + id + ", nome=" + nome + ", preco=" + preco + ", dataLancamento=" + dataLancamento
+				+ ", desenvolvedor=" + desenvolvedor + ", descricao=" + descricao + ", idCategoria=" + idCategoria
+				+ "]";
+	}
+
 }
