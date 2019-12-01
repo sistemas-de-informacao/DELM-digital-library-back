40f3e5419217b10dc3025dd6aaaa18c7a4871dde
Edson Camargo Menezes <42123898+edsoncamargo@users.noreply.github.com>
2019-11-30 22:00:56 -0300

    .
    .

---
+++ src/main/java/br/senac/backend/models/Cart.java
@@ -0,0 +1,45 @@
+package br.senac.backend.models;
+
+import java.util.List;
+
+public class Cart {
+
+	private List<Game> jogos;
+	private String dataCompra;
+	private Double totalCompra;
+	private User comprador;
+	
+	
+	
+	@Override
+	public String toString() {
+		return "Cart [jogos=" + jogos + ", dataCompra=" + dataCompra + ", totalCompra=" + totalCompra + ", comprador="
+				+ comprador + "]";
+	}
+	public List<Game> getJogos() {
+		return jogos;
+	}
+	public void setJogos(List<Game> jogos) {
+		this.jogos = jogos;
+	}
+	public String getDataCompra() {
+		return dataCompra;
+	}
+	public void setDataCompra(String dataCompra) {
+		this.dataCompra = dataCompra;
+	}
+	public Double getTotalCompra() {
+		return totalCompra;
+	}
+	public void setTotalCompra(Double totalCompra) {
+		this.totalCompra = totalCompra;
+	}
+	public User getComprador() {
+		return comprador;
+	}
+	public void setComprador(User comprador) {
+		this.comprador = comprador;
+	}
+	
+	
+}
--- src/main/java/br/senac/backend/models/Historic.java
+++ src/main/java/br/senac/backend/models/Historic.java
@@ -39,6 +39,8 @@ public class Historic {
 		this.idUsuario = idUsuario;
 	}
 	
+
+	
 	
 	
 	
